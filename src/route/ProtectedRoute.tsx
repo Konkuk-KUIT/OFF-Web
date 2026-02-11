a65d9ab267e1ca7f12e1d26b8a54ce600240f792
Baik Hyun Jun <kt997997@gmail.com>
2026-02-12 01:04:27 +0900

    fix/ProtectedRoute파일 리다이렉트하게 수정

--- src/route/ProtectedRoute.tsx
+++ src/route/ProtectedRoute.tsx
@@ -1,6 +1,8 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { isLoggedIn } from "../api/auth";
 
+/*비로그인 시 로그인페이지로 리다이렉트*/
+
 export default function ProtectedRoute() {
   const location = useLocation();
   const loggedIn = isLoggedIn();
